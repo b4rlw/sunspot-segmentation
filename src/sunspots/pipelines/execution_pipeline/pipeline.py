e6993aaa77ee3db34a6e783f0fa7135e5257a010
beatslikeahelix <47506872+beatslikeahelix@users.noreply.github.com>
2022-12-01 14:55:09 +0000

    updated execution pipline w stara modular pipeline

--- src/sunspots/pipelines/execution_pipeline/pipeline.py
+++ src/sunspots/pipelines/execution_pipeline/pipeline.py
@@ -9,6 +9,7 @@ from sunspots.pipelines.data_ingestion import pipeline as di
 from sunspots.pipelines.feature_engineering import pipeline as fe
 from sunspots.pipelines.prediction import pipeline as pr
 from sunspots.pipelines.region_extraction import pipeline as re
+from sunspots.pipelines.STARA import pipeline as st
 
 
 def create_pipeline() -> Pipeline:
@@ -44,9 +45,17 @@ def create_pipeline() -> Pipeline:
         parameters={"params:override_me": "params:box_size"},
         namespace="region_extraction",
     )
+    stara_analysis_pipeline = pipeline(
+        pipe=st.create_pipeline(),
+        inputs={"region_submaps"},
+        outputs={"segmentations", "segmentation_masks", "region_dataframes"},
+        parameters={"params:override_me": "params:stara"},
+        namespace="STARA",
+    )
     return (
         data_ingestion_pipeline
         + feature_engineering_pipeline
         + prediction_pipeline
         + region_extraction_pipeline
+        + stara_analysis_pipeline
     )
