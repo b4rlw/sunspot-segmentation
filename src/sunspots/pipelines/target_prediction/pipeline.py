4c2c0fe8c23446020647c6f0154003adaa4330d2
beatslikeahelix <47506872+beatslikeahelix@users.noreply.github.com>
2022-12-12 01:36:01 +0000

    create pipeline to rename target_prediction

---
+++ src/sunspots/pipelines/target_prediction/pipeline.py
@@ -0,0 +1,21 @@
+"""
+This is a boilerplate pipeline 'target_prediction'
+generated using Kedro 0.18.3
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import predict_dataset_targets
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    prediction_pipeline = pipeline(
+        [
+            node(
+                func=predict_dataset_targets,
+                inputs=["model", "engineered_features", "params:override_me"],
+                outputs="targets",
+                name="predict_dataset_targets",
+            ),
+        ]
+    )
+    return prediction_pipeline
