47830e64dffe7a1f1403b64fb90666d06ecd7f7c
beatslikeahelix <47506872+beatslikeahelix@users.noreply.github.com>
2022-12-13 15:27:21 +0000

    update pipeline for refactored nodes, bring modular pipe back to home folder

--- src/sunspots/pipelines/STARA/pipeline.py
+++ src/sunspots/pipelines/STARA/pipeline.py
@@ -4,17 +4,36 @@ generated using Kedro 0.18.3
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import process_extractions
+from .nodes import stara_process_regions, get_segmentation_masks, get_region_dataframes
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
+    stara_pipeline = pipeline(
         [
             node(
-                func=process_extractions,
-                inputs=["region_submaps", "params:override_me"],
-                outputs=["segmentations", "segmentation_masks", "region_dataframes"],
-                name="process_extractions",
+                func=stara_process_regions,
+                inputs=["region_submaps", "params:stara"],
+                outputs="segmentations",
+                name="stara_process_regions",
+            ),
+            node(
+                func=get_segmentation_masks,
+                inputs="segmentations",
+                outputs="segmentation_masks",
+                name="get_segmentation_masks",
+            ),
+            node(
+                func=get_region_dataframes,
+                inputs=["segmentations", "region_submaps"],
+                outputs="region_dataframes",
+                name="get_region_dataframes",
             ),
         ]
+    )
+    return pipeline(
+        pipe=stara_pipeline,
+        inputs={"region_submaps"},
+        outputs={"segmentation_masks", "region_dataframes"},
+        parameters={"params:stara"},
+        namespace="stara",
     )
